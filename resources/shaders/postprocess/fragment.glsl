b2c87959bd24e637a1898fabe09ada4658372cf4
Emre Kovanci <kovanciemre@gmail.com>
2023-08-17 23:38:14 +0300

    Update fragment.glsl

--- resources/shaders/postprocess/fragment.glsl
+++ resources/shaders/postprocess/fragment.glsl
@@ -1,17 +1,20 @@
 #version 330 core
 
+#define KERNEL_ELEMENT_COUNT 9
+
 in vec2 TexCoords;
+
 out vec4 Color;
 
 uniform sampler2D scene;
-uniform vec2 offsets[9];
-uniform float kernel[9];
+uniform vec2 offsets[KERNEL_ELEMENT_COUNT];
+uniform float kernel[KERNEL_ELEMENT_COUNT];
 
-vec4 applyImageKernel(vec3 samples[9])
+vec4 applyImageKernel(vec3 samples[KERNEL_ELEMENT_COUNT])
 {
 	vec4 result = vec4(0.0);
 
-	for (int i = 0; i < 9; ++i)
+	for (int i = 0; i < KERNEL_ELEMENT_COUNT; ++i)
 	{
 		result += vec4(samples[i] * kernel[i], 0.0);
 	}
