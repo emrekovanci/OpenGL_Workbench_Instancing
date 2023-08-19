4cdd9d3d7110d70ac43ed70ad126059d7ab414aa
Emre Kovanci <kovanciemre@gmail.com>
2023-08-19 04:13:15 +0300

    Update Texture2D.cpp

--- src/Core/Texture2D.cpp
+++ src/Core/Texture2D.cpp
@@ -7,22 +7,22 @@ Texture2D::Texture2D(unsigned int width, unsigned int height, unsigned char* dat
 {
     glGenTextures(1, &Id);
 
+    bind();
+
     if (samples == 0)
     {
-        glBindTexture(GL_TEXTURE_2D, Id);
         glTexImage2D(GL_TEXTURE_2D, 0, InternalFormat, width, height, 0, ImageFormat, GL_UNSIGNED_BYTE, data);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, WrapS);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, WrapT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, FilterMin);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, FilterMax);
-        glBindTexture(GL_TEXTURE_2D, 0);
     }
     else
     {
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, Id);
         glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, InternalFormat, width, height, GL_TRUE);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
     }
+
+    unbind();
 }
 
 Texture2D::~Texture2D()
@@ -37,5 +37,5 @@ void Texture2D::bind() const
 
 void Texture2D::unbind() const
 {
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(Samples == 0 ? GL_TEXTURE_2D : GL_TEXTURE_2D_MULTISAMPLE, 0);
 }
