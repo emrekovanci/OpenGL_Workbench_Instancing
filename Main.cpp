86c6e42c45b7f0c6a95085b72d4007de0378fb9e
Emre Kovanci <kovanciemre@gmail.com>
2023-08-22 02:26:39 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -151,8 +151,6 @@ int main()
     // handle inputs
     window.addEventCallback([&window, &postProcess](const sf::Event& event) {
         if (event.type == sf::Event::Closed) { window.close(); }
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) { window.close(); }
-
         if (event.type == sf::Event::Resized)
         {
             glViewport(0, 0, event.size.width, event.size.height);
